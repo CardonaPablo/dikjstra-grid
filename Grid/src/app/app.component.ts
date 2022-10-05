6bce874fc5348eb805ebe0fab0d44996b504d1dd
Pablo Cardona <pablo@loroinsurance.com>
2022-10-05 13:44:48 -0500

    Full grid generation complete

--- Grid/src/app/app.component.ts
+++ Grid/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatGridList } from '@angular/material/grid-list';
+
+export interface Cell {
+  id: number;
+  status: 'blank' | 'blocked' | 'visited' | 'objective'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,30 +19,57 @@ export class AppComponent {
   dimension = 0
   cells: any[] = []
 
-  initialCellId = 0
-  finalCellId = 0
+  initialCellId: number | null = null
+  finalCellId: number | null = null
 
-  appState: 'point-selection' | 'block-selection' | 'result' = 'point-selection'
+  appState: 'grid-generation' | 'objective-selection' | 'barrier-selection' | 'result' = 'grid-generation'
+
+  dimensionCtrl: FormControl = new FormControl(3, [Validators.required, Validators.min(2), Validators.max(10)])
 
   constructor() {
-    this.renderCells(3);
+    // this.renderCells();
   }
 
-  renderCells(dimension: number): void {
-    this.dimension = dimension
-    this.cells = Array(dimension**2)
+  renderCells(): void {
+    this.dimension = this.dimensionCtrl.value
+    this.cells = Array(this.dimension**2)
     .fill(0)
     .map((v, i) => ({
       id: i+1,
       status: 'blank'
     }))
+    this.appState = 'objective-selection'
   }
 
-  onCellClick(cell: any) {
+
+  onCellClick(cell: Cell) {
     let { status } = cell
-    cell.status = cell.status == 'blocked' ? 'blank' : 'blocked'
+    
+    if(this.appState == 'barrier-selection') {
+      if(this.initialCellId == cell.id || this.finalCellId == cell.id) return
+      cell.status = status == 'blocked' ? 'blank' : 'blocked'
+    }
+
+    if(this.appState == 'objective-selection') {
+      cell.status = 'objective'
+      if(this.initialCellId == null)
+        this.initialCellId = cell.id
+      else if(this.initialCellId != cell.id){
+        this.finalCellId = cell.id
+        this.appState = 'barrier-selection'
+      }
+    }
   }
 
+  restartGrid() {
+    this.initialCellId = null
+    this.finalCellId = null
+    this.appState = 'objective-selection'
+    this.cells = this.cells.map(c => ({ ...c, status: 'blank' }))
+  }
+  
+
+  get validGrid() { return this.initialCellId !== null && this.finalCellId !== null && this.dimensionCtrl.valid }
 }
 
 // TODO: Grid gutter expansion/retractoin animation for loading
